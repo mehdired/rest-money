897156c744f8e6b4ec12b6c2ea97414df1d8c822
redh <mehdi.cheniki@gmail.com>
2025-07-01 17:14:45 +0200

    fix base url auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from 'better-auth/react';
 
 export const { useSession, signIn, signOut, signUp, getSession } = createAuthClient({
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.BETTER_AUTH_URL,
 });
